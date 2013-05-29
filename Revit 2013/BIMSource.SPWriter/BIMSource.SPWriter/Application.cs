49e26de414393f705cb089221a7f94fbe9be45ea
Alan Jackson <a.jackson@case-inc.com>
2013-05-28 22:29:39 -0400

    added ribbon

--- Revit 2013/BIMSource.SPWriter/BIMSource.SPWriter/Application.cs
+++ Revit 2013/BIMSource.SPWriter/BIMSource.SPWriter/Application.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
 using System.Windows.Forms;
-using Autodesk.Revit.ApplicationServices;
+using System.Windows.Media.Imaging;
+
 using Autodesk.Revit.Attributes;
+using Autodesk.Revit.ApplicationServices;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
-using Autodesk.Revit.UI.Selection;
 
 namespace BIMSource.SPWriter
 {
@@ -15,6 +18,14 @@ namespace BIMSource.SPWriter
   [Transaction(TransactionMode.Manual)]
   class Application : IExternalApplication
   {
+    // ExternalCommands assembly path
+    static string AddInPath = typeof(Application).Assembly.Location;
+
+    // Button icons directory
+    static string ButtonIconsFolder = Path.GetDirectoryName(AddInPath);
+
+    public static UIControlledApplication _cachedUiCtrApp;
+
     /// <summary>
     /// Fires off when Revit Session Starts
     /// </summary>
@@ -24,17 +35,16 @@ namespace BIMSource.SPWriter
     {
       try
       {
-        // Add your code here
+        // create customer Ribbon Items
+        CreateRibbonPanel(application);
 
-        // Return Success
-        return Result.Succeeded;
-
+        return Autodesk.Revit.UI.Result.Succeeded;
       }
-      catch
+      catch (Exception ex)
       {
-        // Return Failure
-        return Result.Failed;
+        MessageBox.Show(ex.ToString(), "Ribbon");
 
+        return Autodesk.Revit.UI.Result.Failed;
       }
     }
 
@@ -45,8 +55,36 @@ namespace BIMSource.SPWriter
     /// <returns>Return the status of the external application. A result of Succeeded means that the external application successfully shutdown. Cancelled can be used to signify that the user cancelled the external operation at some point. If false is returned then the Revit user should be warned of the failure of the external application to shut down correctly.</returns>
     public Result OnShutdown(UIControlledApplication application)
     {
-      // Return Success
-      return Result.Succeeded;
+      try
+      {
+
+        //TODO: add you code below.
+
+
+        return Result.Succeeded;
+      }
+      catch (Exception ex)
+      {
+        MessageBox.Show(ex.ToString());
+        return Result.Failed;
+      }
+    }
+
+    private RibbonPanel CreateRibbonPanel(UIControlledApplication application)
+    {
+      // create a Ribbon panel which contains three stackable buttons and one single push button.
+      string firstPanelName = "BIM Source";
+      RibbonPanel ribbonPanel = application.CreateRibbonPanel(firstPanelName);
+
+      //Create First button:
+      PushButtonData pbDataAddParameterToFamily = new PushButtonData("AddParameterToFamily", "Bind Parameters \nTo Family",
+          AddInPath, "BIMSource.SPWriter.Commands");
+      PushButton pbAddParameterToFamily = ribbonPanel.AddItem(pbDataAddParameterToFamily) as PushButton;
+      pbAddParameterToFamily.ToolTip = "Bind Shared Parameters From External File to Family";
+      pbAddParameterToFamily.LargeImage = new BitmapImage(new Uri(Path.Combine(ButtonIconsFolder, "BIMSource.SPWriter.32.png"), UriKind.Absolute));
+      pbAddParameterToFamily.Image = new BitmapImage(new Uri(Path.Combine(ButtonIconsFolder, "BIMSource.SPWriter.16.png"), UriKind.Absolute));
+
+      return ribbonPanel;
     }
   }
 }
