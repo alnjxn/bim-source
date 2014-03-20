21ab71123ab7564859da716f9bc54737496dbb92
Alan Jackson <a.jackson@case-inc.com>
2014-03-20 15:19:45 -0400

    overhaul to new version

--- Revit 2014/BIMSource.SPWriter/BIMSource.SPWriter/Properties/AssemblyInfo.cs
+++ Revit 2014/BIMSource.SPWriter/BIMSource.SPWriter/Properties/AssemblyInfo.cs
@@ -1,16 +1,18 @@
 ﻿using System.Reflection;
+using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Windows;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Revit 2014 BIM Source SP.Witer")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("BIMSource.SPWriter")]
+[assembly: AssemblyDescription("SP.Writer for Autodesk Revit 2014")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("BIM Source")]
-[assembly: AssemblyProduct("Revit 2014 BIM Source SP.Witer")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyProduct("BIMSource.SPWriter")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -19,8 +21,25 @@ using System.Runtime.InteropServices;
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("f2b9f5eb-3641-4b4a-9510-93f5048e6ee9")]
+//In order to begin building localizable applications, set 
+//<UICulture>CultureYouAreCodingWith</UICulture> in your .csproj file
+//inside a <PropertyGroup>.  For example, if you are using US english
+//in your source files, set the <UICulture> to en-US.  Then uncomment
+//the NeutralResourceLanguage attribute below.  Update the "en-US" in
+//the line below to match the UICulture setting in the project file.
+
+//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
+
+
+[assembly: ThemeInfo(
+    ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
+    //(used if a resource is not found in the page, 
+    // or application resource dictionaries)
+    ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
+    //(used if a resource is not found in the page, 
+    // app, or any theme specific resource dictionaries)
+)]
+
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +51,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2012.0.0.0")]
-[assembly: AssemblyFileVersion("2012.0.0.0")]
+[assembly: AssemblyVersion("2014.3.20.0")]
+[assembly: AssemblyFileVersion("2014.2.20.0")]
+[assembly: GuidAttribute("cd4773a9-0249-49f3-a624-6bf5bbfd5d5c")]
+[assembly: NeutralResourcesLanguageAttribute("en")]
